14c303757fbc320b74591645df162346a023bead
Glitch (file-metadata-microservice-project-fcc) <none>
2018-09-27 19:31:49 +0000

    🍢🏍 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer');
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, err) =>{
+  console.log(req.file);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
